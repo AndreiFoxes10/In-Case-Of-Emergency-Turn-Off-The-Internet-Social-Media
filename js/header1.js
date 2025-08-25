a57cea3fd0cd185215ba447c7e5b7041b2559fb5
AndreiFoxes10 <71722314+AndreiFoxes10@users.noreply.github.com>
2025-08-25 23:20:57 +0300

    Add files via upload

--- js/header1.js
+++ js/header1.js
@@ -1,5 +1,8 @@
 document.querySelector(".writeHeader").innerHTML = `
-    <img src="img/1.png" alt="Logo" width="100" height="100"> 
+    <img src="img/9.png" alt="Logo" width="100" height="100"> 
     <div><b>In Case Of Emergency Turn Off The Internet/Social Media</b></div>
     <div><b>by <a href="https://www.youtube.com/@AndreiFoxes10">@AndreiFoxes10</a></b></div>
+    <nav>
+        <div><h1><a href="index.html">Home</a> <a href="video.html">Video</a></h1></div>
+    </nav>
 `;
